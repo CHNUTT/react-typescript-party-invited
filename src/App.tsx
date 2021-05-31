33e21fbee21a2b9767b7ddd0a8d4d90be1c75e0e
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-05-31 10:51:26 -0500

    [ Type definition ] - defind people type definition in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// type People = {
+//   name: string,
+//   avatar: string,
+//   age: number,
+//   note?: string
+// }
+
+interface IState {
+	people: {
+		name: string;
+		avatar: string;
+		age: number;
+		note?: string;
+	}[];
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [people, setPeople] = useState<IState["people"]>([]);
+
+	return (
+		<div className='App'>
+			<h1>People Invited to my Part</h1>
+		</div>
+	);
 }
 
 export default App;
