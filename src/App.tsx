3faec985829cc27e2c37450a4894ecae3e2810a2
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-05-31 11:17:01 -0500

    [ Define return type of the function ] - implement rendering list function with in the component and define the renturn type

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,26 @@ interface IState {
 }
 
 function App() {
-	const [people, setPeople] = useState<IState['people']>([]);
+	const [people, setPeople] = useState<IState['people']>([
+		{
+			name: 'Chad',
+			avatar: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+			age: 30,
+			note: 'first one',
+		},
+		{
+			name: 'John',
+			avatar:
+				'https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg',
+			age: 29,
+			note: 'second one',
+		},
+	]);
 
 	return (
 		<div className='App'>
 			<h1>People Invited to my Part</h1>
-      <List people={people} />
+			<List people={people} />
 		</div>
 	);
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,7 +8,19 @@ interface IProps {
 }
 
 const List: React.FC<IProps> = ({ people }) => {
-	return <div>I am a List</div>;
+	const renderList = (): JSX.Element[] =>
+		people.map((person) => (
+			<li className='List'>
+				<div className='List-header'>
+					<img className='List-img' src={person.avatar} alt={person.name} />
+					<h2>{person.name}</h2>
+				</div>
+				<p>{person.age}</p>
+				<p className='List-note'>{person.note}</p>
+			</li>
+		));
+
+	return <ul>{renderList()}</ul>;
 };
 
 export default List;
